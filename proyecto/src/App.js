8e8c5d203825b706f2d4e5820795dc64ab4eba8d
fsilva <fsilva@43M4KD3>
2021-11-23 13:26:39 -0600

    Initialize project using React and Tailwind

---
+++ proyecto/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Post from './components/Post/Post';
+
+function App() {
+  return (
+    <div className="App">
+      <Post/>
+    </div>
+  );
+}
+
+export default App;
---
+++ proyecto/src/components/Post/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post = () => {
+
+    return (
+        <div className="bg-red-600">
+            <h1>Test de Post</h1>
+        </div>
+    )
+}
+
+export default Post;
