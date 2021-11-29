159b35e88874b2d4646143778a7fe0772e19efb5
Andres Puente <00287919@uca.edu.sv>
2021-11-28 22:33:08 -0600

    added deactivation button and functionality + more

--- proyecto/src/components/NewPostForm.jsx
+++ proyecto/src/components/NewPostForm.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import { useUserContext } from '../contexts/UserContext';
 import services from '../services/services';
 
-const NewPostForm = ({setReload}) => {
-    const [error, setError] = useState(false);
+const NewPostForm = ({ setReload }) => {
+    const [error1, setError1] = useState(false);
+    const [error2, setError2] = useState(false);
+    const [error3, setError3] = useState(false);
     const { token } = useUserContext();
 
     const onSubmit = async (e) => {
@@ -11,26 +13,42 @@ const NewPostForm = ({setReload}) => {
 
         const formData = new FormData(e.target);
         const data = Object.fromEntries(formData.entries());
+        
+        setError1(false);
+        setError2(false);
+        setError3(false);
 
         if (data.title === '' || data.description === '' || data.image === '') {
-            setError(true);
+            setError1(true);
             return;
         }
-        setError(false);
+        if (data.title.length < 8 || data.title.length > 32) {
+            setError2(true);
+            return;
+        }
+        if (data.description.length < 8) {
+            setError3(true);
+            return;
+        }
+
         console.log(data);
         const response = await services.create(token, data);
         console.log(response);
-        if (response) {
-            document.getElementById("new-post").reset();
-            setReload(true);
-        }
+        if (!response) return;
+        document.getElementById("new-post").reset();
+        setReload(true);
     }
 
     return (
         <form name="new-post" id="new-post" className="bg-white flex flex-col p-2 mb-2 items-center"
             onSubmit={onSubmit}
         >
-            {error && <p>Todos los campos son requeridos!</p>}
+            <div className="text-red-600">
+                {error1 && <p>Todos los campos son requeridos!</p>}
+                {error2 && <p>Titulo debe ser entre 8 y 32 caracteres de largo.</p>}
+                {error3 && <p>Descripcion debe ser minimo 8 caracteres de largo.</p>}
+            </div>
+
             <input className="bg-gray-100 px-3 py-1 mb-2 w-4/5 text-sm rounded-full placeholder-gray-800"
                 type="text"
                 name="title"
--- proyecto/src/components/PostContainer.jsx
+++ proyecto/src/components/PostContainer.jsx
@@ -3,38 +3,36 @@ import services from '../services/services';
 import { useUserContext } from '../contexts/UserContext';
 import Post from './Post';
 
-const PostContainer = ( {filters,reload,setReload} ) => {
+const PostContainer = ({ filters, reload, setReload }) => {
     const [rawData, setRawData] = useState({});
     const { token, user } = useUserContext();
     const [userFavs, setUserFavs] = useState({});
 
-    
-
     useEffect(() => {
-        if(reload){
+        if (reload) {
             const getAll = async () => {
                 const raw = await services.getAll(token, filters);
                 setRawData(raw);
-    
-                const rawFavs = await services.getFavs(token); 
+
+                const rawFavs = await services.getFavs(token);
                 setUserFavs(rawFavs);
                 //console.log(userFavs);
             }
             getAll();
             setReload(false);
         }
-        
-    }, [filters,reload]);
 
+    }, [filters, reload]);
 
     return (
         <div className="flex flex-col justify-center 
             items-center gap-2 md:gap-4">
             {
-                rawData.data && userFavs.favorites && rawData.data.map( (post) => 
+                rawData.data && userFavs.favorites && rawData.data.map((post) =>
                     <Post loggedUser={user} key={post._id} data={post} token={token} userFavs={userFavs.favorites.some((x) => x === post._id)} setReload={setReload} />
                 )
             }
+            
         </div>
     )
 }
